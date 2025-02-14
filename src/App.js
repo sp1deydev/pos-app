6dd46e884ae414c4239e29b95772203bfdd3712d
sp1deydev <duckhanhthientran@gmail.com>
2025-02-14 15:03:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Router />
       </BrowserRouter>
     </div>
